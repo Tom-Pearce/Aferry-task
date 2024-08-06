53d8525c0005227c98b49e9e87424c18eeee320e
Tom Pearce <thomas.pearce@openstudycollege.com>
2024-08-06 17:29:38 +0100

    feat: added a health check for the external service

--- src/utils/external-service.ts
+++ src/utils/external-service.ts
@@ -1,2 +1,15 @@
 const EXTERNAL_SERVICE_URL = process.env.PUBLISH_URL;
 
+export const healthCheck = async () => {
+    if (!EXTERNAL_SERVICE_URL) {
+        return false;
+    }
+
+    try {
+        const response = await fetch(EXTERNAL_SERVICE_URL);
+        return response.ok;
+    } catch {
+        // TODO: Log unavailability
+        return false;
+    }
+};
